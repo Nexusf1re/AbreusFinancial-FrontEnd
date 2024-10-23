2fed666d229cb05cc79381d890e4a76f444ef825
Matteus <matteussantos30@gmail.com>
2024-10-23 08:29:01 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import First from './First';
+import Logo from './Logo';
 
 function App() {
   return (
     <div className="App">
       
-      
+      <First />
+      <Logo />
 
     </div>
   );
---
+++ src/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Logo.css'
+
+function Logo(){
+    return <img src='/logo512.png'  alt='logo'/>;
+}
+
+export default Logo;
