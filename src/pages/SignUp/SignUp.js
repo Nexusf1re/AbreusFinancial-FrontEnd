856f607d467e775eabbe3e5ede59a44fa67f2f71
Matteus Abreu <matteussantos30@gmail.com>
2024-10-24 20:57:12 -0300

    sign up page maked

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,26 +1,43 @@
-import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import './Login.css';
 import Slogan from '../../assets/Slogan.png'; 
 import { FaLock } from "react-icons/fa6";
 import { FaRegEnvelope } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa6";
+import './SignUp.css';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log('User:', username)
     console.log('Email:', email);
     console.log('Password:', password);
+    console.log('ConfirmPassword:', confirmPassword);
   };
 
   return (
     <div className="wrapper">
       <img src={Slogan} alt="Logo" />
-      <h1>Login</h1>
-      <h3 id='controle'>Controle Financeiro</h3>
+      <h1>Cadastre-se</h1>
+     <h3 id='controle'>Controle Financeiro</h3>
       <form onSubmit={handleSubmit}>
+
+      <div className="input-box">
+         <FaRegUser  className='icon'/>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Seu nome"
+            required
+          />
+        </div>
+
         <div className="input-box">
         <FaRegEnvelope className='icon'/>
           <input
@@ -33,6 +50,7 @@ const Login = () => {
           />
         </div>
         <div className="input-box">
+
         <FaLock className='icon'/>
           <input
             type="password"
@@ -43,10 +61,23 @@ const Login = () => {
             required
           />
         </div>
-        <button type="submit" className="btn">Entrar</button>
+
+        <div className="input-box">
+        <FaLock className='icon'/>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirmar Senha"
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn">Cadastrar</button>
       </form>
       <div id="cadastrar">
-        <a href="/sign-up"><p>Não tem uma conta?</p> Cadastre-se</a>
+        <a href="/">Login</a>
       </div>
     </div>
   );
