a9a0eb6c61a0aba08eaca5f6153355e9e2432024
Matteus <matteussantos30@gmail.com>
2024-10-26 08:39:06 -0300

    BottomBar href and Css Fixed

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { FaHouse, FaBrazilianRealSign, FaFileInvoiceDollar, FaGear } from "react-icons/fa6";
-import styles from './BottomBar.module.css'; // Alterar para CSS Module
+import styles from './BottomBar.module.css';
 
 const BottomBar = () => {
-    return (
-        <div className={styles.bottombar}>
-            <a><span><FaHouse className={styles.home}/></span> Início</a>
-            <a><span><FaBrazilianRealSign className={styles.lancar}/></span>Lançar</a>
-            <a><span><FaFileInvoiceDollar className={styles.historico}/></span>Histórico</a>
-            <a><span><FaGear className={styles.config}/></span>Config</a>
-        </div>
-    );
+return (
+<div className={styles.bottombar}>
+    <a href='/home'><span><FaHouse className={styles.home}/></span> Início</a>
+    <a href='/form'><span><FaBrazilianRealSign className={styles.lancar}/></span>Lançar</a>
+    <a href='/history'><span><FaFileInvoiceDollar className={styles.historico}/></span>Histórico</a>
+    <a href='/config'><span><FaGear className={styles.config}/></span>Config</a>
+</div>
+);
 };
 
 export default BottomBar;
