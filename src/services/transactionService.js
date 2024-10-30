e40f627fd554a2dd9ead94edfd00de9c78730f41
Matteus <matteussantos30@gmail.com>
2024-10-30 09:50:34 -0300

    transactions list improved

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchFinancialData = async () => {
-    const username = localStorage.getItem("username");
-    if (!username) {
-        throw new Error('Usuário não encontrado no localStorage');
+    const token = localStorage.getItem("token");
+    if (!token) {
+        throw new Error('Token não encontrado no localStorage');
     }
 
     try {
-        const response = await axios.get(`${API_URL}/transactions/financial?Username=${username}`);
+        const response = await axios.get(`${API_URL}/transactions/financial`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         throw new Error('Erro ao buscar dados financeiros: ' + error.message);
