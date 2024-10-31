50b6a7a08a81980544f56145ae7a4394def7b9e5
Matteus Abreu <matteussantos30@gmail.com>
2024-10-31 00:37:59 -0300

    count up animation home added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from "./Home.module.css";
 import TopBar from '../../components/TopBar/TopBar';
 import BottomBar from '../../components/BottomBar/BottomBar';
-import useFinanceData from '../../hooks/homeData'; // Corrigido o nome do hook
+import useFinanceData from '../../hooks/homeData';
+import CountUp from 'react-countup';
 
 const Home = () => {
-  const { totalEntrada, totalSaida, balancoMes } = useFinanceData(); 
+  const { totalEntrada, totalSaida, balancoMes } = useFinanceData();
 
   const date = new Date().toLocaleDateString('pt-BR', {
     day: 'numeric',
@@ -24,24 +25,49 @@ const Home = () => {
       <div className={styles.InOut}>
         <div className={`${styles.income} ${styles.card}`}>
           <p className={styles.name}>Total Entrada</p>
-          <p className={styles.icomeValue}>R${totalEntrada.toFixed(2).replace('.', ',')}</p>
+          <p className={styles.icomeValue}>
+            R$
+            <CountUp
+              end={totalEntrada}
+              duration={1} // Duração da animação em segundos
+              decimals={2}
+              decimal=","
+              separator="."
+            />
+          </p>
         </div>
 
         <div className={`${styles.outgoing} ${styles.card}`}>
           <p className={styles.name}>Total Saída</p>
-          <p className={styles.outgoingValue}>R${totalSaida.toFixed(2).replace('.', ',')}</p>
+          <p className={styles.outgoingValue}>
+            R$
+            <CountUp
+              end={totalSaida}
+              duration={1}
+              decimals={2}
+              decimal=","
+              separator="."
+            />
+          </p>
         </div>
       </div>
 
       <div className={`${styles.monthBalance} ${styles.card}`}>
         <p className={styles.name}>Balanço Mês</p>
         <p className={`${styles.balanceValue} ${balancoMes < 0 ? styles.negative : styles.positive}`}>
-          R${balancoMes.toFixed(2).replace('.', ',')}
+          R$
+          <CountUp
+            end={balancoMes}
+            duration={1.5}
+            decimals={2}
+            decimal=","
+            separator="."
+          />
         </p>
       </div>
 
       <div className={`${styles.graph} ${styles.card}`}>
-      
+        {/* Gráfico ou conteúdo adicional */}
       </div>
 
       <BottomBar />
